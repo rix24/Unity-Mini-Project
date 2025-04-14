c007efafaea6e76837f4fdd369090a4d9dc38f4c
elvysia <124395523+elvysia@users.noreply.github.com>
2025-04-14 15:14:02 +0100

    Scene Loader
    Created scene loader script and prefab
    Applied functions to buttons
    Unsure on quit function

---
+++ Assets/Elvysia/Scripts/SceneManager/SceneLoader.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        
+    }
+}
